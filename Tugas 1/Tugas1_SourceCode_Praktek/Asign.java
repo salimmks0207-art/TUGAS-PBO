820bef2175c14c484e6d5089080ffd3d39189b83
salimmks0207-art <salimmks0207@gmail.com>
2026-04-05 16:22:05 +0800

    Add files via upload

---
+++ Tugas 1/Tugas1_SourceCode_Praktek/Asign.java
@@ -0,0 +1,13 @@
+public class Asign {
+	//NIM	: 13020240076
+	//NAMA	: Muh. Syarif Hidayatullah
+	//Minggu/05-04-2026. 16:06
+
+	public static void main (String[] args){
+
+	int i;
+	System.out.print("Hello\n"); i = 5;
+	System.out.println("Ini nilai i : " + i);
+}
+
+}
