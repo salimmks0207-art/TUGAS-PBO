3d888f85188470f0caa62686ccf417bba6aeaa3f
salimmks0207-art <salimmks0207@gmail.com>
2026-04-05 15:37:19 +0800

    Add files via upload

---
+++ Tugas 1/Asgdll.java
@@ -0,0 +1,13 @@
+public class Asgdll {
+	//NIM	: 13020240076
+	//NAMA	: Muh. Syarif Hidayatullah
+	//Minggu/05-04-2026
+	
+	public static void main (String[] args) {
+	float f = 20.0f;
+	double fll;
+	fll = 10.0f;
+	System.out.println("f : "+ f + "\nfll : "+ fll);
+}
+
+}
